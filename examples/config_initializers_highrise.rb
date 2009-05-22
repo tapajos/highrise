77c10a79a9e1cdd43b50f8dd2c0415c1616afc7b
Kenneth Mayer <kmayer@bitwrangler.com>
2009-05-22 13:28:16 -1000

    Version bump to 0.11.1

---
+++ examples/config_initializers_highrise.rb
@@ -0,0 +1,11 @@
+if Rails.env != 'test' then
+  Highrise::Base.site = 'https://example.com.i'
+  # For backward compatability
+  if Highrise::Base.respond_to? :user
+    Highrise::Base.user = 'my_fancy_auth_token'
+  else 
+    Highrise::Base.site = 'https://my_fancy_auth_token@example.com.i'
+  end
+  # The cache store can be anything that ActiveSupport can handle
+  Highrise::Base.connection.cache_store = ActiveSupport::Cache.lookup_store
+end
