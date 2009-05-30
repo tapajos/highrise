fd128e807eb2a03f233272aa446671c68844322e
Kenneth Mayer <kmayer@bitwrangler.com>
2009-05-29 22:34:03 -1000

    Added Tag.find_by_name Version bump to 0.13.0

--- lib/highrise/tag.rb
+++ lib/highrise/tag.rb
@@ -1,7 +1,18 @@
 module Highrise
   class Tag < Base
+    @@tags = {}
     def ==(object)
       (object.instance_of?(self.class) && object.id == self.id && object.name == self.name)
+    end
+    
+    def self.find_by_name(arg)
+      if @@tags == {}
+        self.find(:all).each do |tag|
+          @@tags[tag.id] = tag
+          @@tags[tag.name] = tag
+        end
+      end
+      @@tags[arg]
     end
   end
 end
