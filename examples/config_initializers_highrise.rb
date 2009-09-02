34bd8cf618fb1e90a5d30cbd6629eb87c6883be1
Kenneth Mayer <kmayer@bitwrangler.com>
2009-09-02 08:56:42 -1000

    Version bump to 1.0.1

--- examples/config_initializers_highrise.rb
+++ examples/config_initializers_highrise.rb
@@ -7,6 +7,6 @@ if Rails.env != 'test' then
     Highrise::Base.site = 'https://my_fancy_auth_token@example.com.i'
   end
   # The cache store can be anything that ActiveSupport can handle
-  Highrise::Base.connection.cache_store = ActiveSupport::Cache.lookup_store :mem_cache_store
-  Highrise::Base.connection.store_options = { :expires_in => 60.seconds }
+  Highrise::Base.cache_store = ActiveSupport::Cache.lookup_store :mem_cache_store
+  Highrise::Base.store_options = { :expires_in => 60.seconds }
 end
--- examples/sample.rb
+++ examples/sample.rb
@@ -3,7 +3,7 @@ require 'pp'
 
 Highrise::Base.site = 'https://yoursite.highrisehq.com'
 Highrise::Base.user = 'xxx'
-Highrise::Base.connection.cache_store = :memory_store
+Highrise::Base.cache_store = :memory_store
 
 @tags = Highrise::Tag.find(:all)
 
