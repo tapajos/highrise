9208bda4184b7b410401f3b20c56edca78ff7d80
Denis Odorcic <denis.odorcic@gmail.com>
2010-10-14 11:48:21 +0800

    Add Kase.open and Kase.closed functions

--- lib/highrise/kase.rb
+++ lib/highrise/kase.rb
@@ -4,5 +4,13 @@ module Highrise
       self.closed_at = Time.now.utc
       save
     end
+
+    def self.open
+      Kase.find(:all, :from => "/kases/open.xml")
+    end
+
+    def self.closed
+      Kase.find(:all, :from => "/kases/closed.xml")
+    end
   end
 end
