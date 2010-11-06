57ed823b85b4d58dcaf47c3847e40573347ce494
Kenneth Mayer <kmayer@bitwrangler.com>
2010-11-06 14:58:41 -0700

    Added pagination & find all since
    via https://github.com/odorcicd/highrise/commit/6224a4779cec89a6263d885d0ce1241757da8ce4

--- lib/highrise/recording.rb
+++ lib/highrise/recording.rb
@@ -1,3 +1,9 @@
 module Highrise
-  class Recording < Base; end
+  class Recording < Base
+    include Pagination
+
+    def self.find_all_across_pages_since(time)
+      find_all_across_pages(:params => { :since => time.utc.strftime("%Y%m%d%H%M%S") })
+    end
+  end
 end
--- spec/highrise/recording_spec.rb
+++ spec/highrise/recording_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe Highrise::Recording do
   it { should be_a_kind_of Highrise::Base }
+
+  it_should_behave_like "a paginated class"
+  
+  it ".find_all_across_pages_since" do
+    time = Time.parse("Wed Jan 14 15:43:11 -0200 2009")
+    Highrise::Recording.should_receive(:find_all_across_pages).with({:params=>{:since=>"20090114174311"}}).and_return("result")
+    Highrise::Recording.find_all_across_pages_since(time).should == "result"
+  end
 end
