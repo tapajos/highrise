db5ed3cf38885be8a479f8ed36a084f398d73007
Kenneth Mayer <kmayer@bitwrangler.com>
2010-11-10 06:03:35 -0800

    Always use SSL transport (Highrise API changes)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ Bundler.setup
 require File.join(File.dirname(__FILE__), '/../lib/highrise')
 
 Highrise::Base.user = ENV['HIGHRISE_USER'] || 'x'
-Highrise::Base.site = ENV['HIGHRISE_SITE'] || 'http://www.example.com'
+Highrise::Base.site = ENV['HIGHRISE_SITE'] || 'https://www.example.com'
 
 require 'highrise/pagination_behavior'
 require 'highrise/searchable_behavior'
