4fffaef5a10b86f259bdf6599116b1cb74823655
Joshua Coffee <josh@joffeedesign.com>
2014-04-05 00:02:23 -0500

    fixes undefined method issue when untag! has be called

--- lib/highrise/taggable.rb
+++ lib/highrise/taggable.rb
@@ -9,8 +9,8 @@ module Highrise
     end    
 
     def untag!(tag_name)
-      to_delete = self.tags.find{|tag| tag['name'] == tag_name} unless tag_name.blank?
-      self.untag_id!(to_delete['id']) unless to_delete.nil?
+      to_delete = self.tags.find{|tag| tag.attributes['name'] == tag_name} unless tag_name.blank?
+      self.untag_id!(to_delete.attributes['id']) unless to_delete.nil?
     end
     protected
     def untag_id!(tag_id)
