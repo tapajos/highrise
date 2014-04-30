2e0e6a8635f6a5f3cd4a650954d8df43c7634496
Bas Vodde <basv@odd-e.com>
2014-04-30 21:41:16 +0800

    Equal operator for SubjectData

--- lib/highrise/subject_data.rb
+++ lib/highrise/subject_data.rb
@@ -1,4 +1,8 @@
 module Highrise
   class SubjectData < Base
+    def==other
+      attributes["value"] == other.attributes["value"] &&
+      attributes["subject_field_label"] == other.attributes["subject_field_label"]
+    end
   end
 end
