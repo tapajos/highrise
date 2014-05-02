2cb1ec5448ad4dd777b4cee531a32c666de00d59
Bas Vodde <basv@odd-e.com>
2014-05-02 11:24:08 +0800

    Adding cache to SubjectField. They rarely change and when not caching then it will hit the network every time you set a field on a new person

--- lib/highrise/subject_field.rb
+++ lib/highrise/subject_field.rb
@@ -1,4 +1,11 @@
 module Highrise
   class SubjectField < Base
+    def self.find_every(options)
+      @subject_field_cache ||= super
+    end
+    
+    def self.invalidate_cache
+      @subject_field_cache = nil
+    end
   end
 end
--- spec/highrise/subject_field_spec.rb
+++ spec/highrise/subject_field_spec.rb
@@ -2,4 +2,38 @@ require 'spec_helper'
 
 describe Highrise::SubjectField do
   it { should be_a_kind_of Highrise::Base }
+    
+  it "Should cache the find all to prevent too much queries for the SubjectFields" do
+    
+    subject_fields = [ Highrise::SubjectField.new({:id => 1, :label => "Cabbage"}), 
+                        Highrise::SubjectField.new({:id => 2, :label => "Chicken"})]
+    
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get    "/subject_fields.xml", {"Authorization"=>"Bearer OAUTH_TOKEN", "Accept"=>"application/xml"}, subject_fields.to_xml
+    end
+        
+    Highrise::SubjectField.find(:all).length.should==2
+
+    ActiveResource::HttpMock.respond_to do |mock|      
+    end
+
+    Highrise::SubjectField.find(:all).length.should==2
+  end
+  
+  it "Should be able to invalidate the cache" do
+    subject_fields = [ Highrise::SubjectField.new({:id => 1, :label => "Cabbage"}), 
+                       Highrise::SubjectField.new({:id => 2, :label => "Chicken"})]
+    
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get    "/subject_fields.xml", {"Authorization"=>"Bearer OAUTH_TOKEN", "Accept"=>"application/xml"}, subject_fields.to_xml
+    end
+        
+    Highrise::SubjectField.find(:all)
+
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get    "/subject_fields.xml", {"Authorization"=>"Bearer OAUTH_TOKEN", "Accept"=>"application/xml"}, [].to_xml
+    end
+    Highrise::SubjectField.invalidate_cache
+    Highrise::SubjectField.find(:all).length.should==0
+  end
 end
